1bcd6cb8f0b0383ce909443122586d0d9d29a207
Jiangliming <563190765@qq.com>
2020-07-09 14:12:48 +0800

    数字数的加单网页

---
+++ wordcount/function.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""code_info
+@Time    : 2020 2020/7/8 13:06
+@Author  : jiangliming
+@File    : function.py
+"""
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    user_text = request.GET['text']
+    total_count = len(user_text)
+
+    word_dict = {}
+    for word in user_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    sorted_dict = sorted(word_dict.items(), key=lambda w: w[1], reverse=True)
+
+    return render(request, 'count.html',
+                  {'count': total_count, 'text': user_text,
+                   'wordict': word_dict ,'sortedict':sorted_dict})
+
+
+def about(request):
+    return render(request, 'about.html')
